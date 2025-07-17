799df2fff5dcb5756d709fe51ca1e0283db7e6b5
Oo <KaungMyat.Oo@cantor.com>
2025-07-17 16:18:58 -0400

    Remove MoreValidation testcase 3 from @Reg

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -34,12 +34,14 @@ test('@API Screenshot & Visual comparision', async ({ page }) => {
     await expect(page.locator("#displayed-text")).toBeHidden();
 });
 //screenshot -store -> screenshot -> 
-test(async ({ page }) => {
+/*
+test('Screenshot & Visual comparision',(async ({ page }) => {
     //make payment -when you 0 balance
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     expect(await page.screenshot()).toMatchSnapshot('landing-win32.png');
 
-})
+})) */
+    ;
 
 
 
