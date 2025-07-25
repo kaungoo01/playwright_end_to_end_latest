1e72cbae051cdc7efb083148106f16eb097e2b6d
Oo <KaungMyat.Oo@cantor.com>
2025-07-25 08:15:45 -0400

    CartPage and starter

--- pageObjects/CartPage.js
+++ pageObjects/CartPage.js
@@ -1,31 +1,28 @@
 const { expect } = require('@playwright/test');
+
 class CartPage {
-
     constructor(page) {
-
         this.page = page;
-        //locators
-        //Product
+        // Locators
         this.cartProducts = page.locator("div li").first();
-       // this.cartProducts = page.locator("div[class='cartSection'] h3").first();
-        //Checkout button
+        // this.cartProducts = page.locator("div[class='cartSection'] h3").first();
         this.checkout = page.locator("text=Checkout");
     }
 
-    //Methods
+    // Methods
     async VerifyProductIsDisplayed(productName) {
         const productLocator = this.getProductLocator(productName);
-        await expect(productLocator).toBeVisible(); // Cleaner & uses built-in timeout
+        await expect(productLocator).toBeVisible();
         console.log(`Product name is : ${productName}`);
     }
-
 
     async Checkout() {
         await this.checkout.click();
     }
 
     getProductLocator(productName) {
-        return this.page.locator("h3:has-text('" + productName + "')"); //Original locator ("h3:has-text('zara coat 3')")
+        return this.page.locator(`h3:has-text('${productName}')`);
     }
 }
+
 module.exports = { CartPage };
---
+++ tests/starterCode.spec.js
@@ -0,0 +1,8 @@
+const { test, expect, request } = require('@playwright/test');
+
+test('Fill in your Test Case Name', async ({ page }) => //plain page only
+//code
+{
+    console.log('🦾🦾🦾🦾 Lets Getting Started! ');
+
+    });
