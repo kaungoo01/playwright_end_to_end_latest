c916cd16a07395bde2a64b394d06c8c42fd9aca7
Oo <KaungMyat.Oo@cantor.com>
2025-08-06 20:08:37 -0400

    Cart Page 8/6/25

--- pageObjects/CartPage.js
+++ pageObjects/CartPage.js
@@ -3,7 +3,7 @@ const { expect } = require('@playwright/test');
 class CartPage {
     constructor(page) {
         this.page = page;
-        // Locators
+        // Locators 1st
         this.cartProducts = page.locator("div li").first();
         // this.cartProducts = page.locator("div[class='cartSection'] h3").first();
         this.checkout = page.locator("text=Checkout");
